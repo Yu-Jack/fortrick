27496a7b183f053b509dd1ed915c6c50162e1f86
Yu-Jack <jk82421@gmail.com>
2016-01-23 19:15:38 +0800

    First

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+var config = {
+    entry: [path.resolve(__dirname, 'app/src/layout.jsx')],
+    output: {
+        path: path.resolve(__dirname, 'app/build/'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /.jsx$/,
+            exclude: /node_modles/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015','react']
+            }
+        }]
+    }
+};
+
+module.exports = config;
